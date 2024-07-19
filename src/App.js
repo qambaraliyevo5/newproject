f0db6e9767845813e34d620b580f1a0551504359
Nodirbekakm2205 <nodirbekakm2205@gmail.com>
2024-07-19 21:12:42 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout_Admin from './components/admin/admin__layout/layout_admin';
+import Login from './login/login';
 
 function App() {
   <BrowserRouter>
   <Routes>
-    <Route path='/admin' element={<Admin_Layout/>}/>
+    <Route path='/admin' element={<Layout_Admin/>}/>
+    <Route path='/login' element={<Login/>}/>
+
   </Routes>
   </BrowserRouter>
 }
